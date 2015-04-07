1e94a42a3972c0ce56cfdd242c8f226a3f538bef
nwgat <github@nwgat.net>
2015-04-07 14:01:27 +0200

    cleaning unused stuff

--- nodejs-rtorrent-installer-ubuntu.sh
+++ nodejs-rtorrent-installer-ubuntu.sh
@@ -1,14 +1,9 @@
 # Colors
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_YELLOW=$ESC_SEQ"33;01m"
 COL_BLUE=$ESC_SEQ"34;01m"
-COL_MAGENTA=$ESC_SEQ"35;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
 
-echo -e "$COL_RED ### Nwgat NodejS-rTorrent Installer ### $COL_RESET"
+echo -e "$COL_BLUE ### Nwgat NodejS-rTorrent Installer ### $COL_RESET"
 echo "This will install packages and create rtorrent user"
 echo "Codename Exia"
 echo ""
@@ -29,7 +24,7 @@ apt-get install -y python g++ make nodejs rtorrent git supervisor ufw > /dev/nul
 ufw allow 3000
 
 echo ""
-echo -e "$COL_YELLOW Packages Installed $COL_RESET"
+echo -e "$COL_BLUE Packages Installed $COL_RESET"
 echo ""
 
 adduser --disabled-password --gecos "" rtorrent > /dev/null
@@ -41,7 +36,7 @@ echo "session = /home/rtorrent/.session/" >> /home/rtorrent/.rtorrent.rc
 mkdir /home/rtorrent/.session/
 
 echo ""
-echo -e "$COL_YELLOW rTorrent Configured $COL_RESET"
+echo -e "$COL_BLUE rTorrent Configured $COL_RESET"
 echo ""
 
 git clone https://github.com/roastlechon/nodejs-rtorrent.git /home/rtorrent/nodejs-rtorrent
@@ -56,7 +51,7 @@ chown -R rtorrent:rtorrent /home/rtorrent
 su rtorrent -c "cd /home/rtorrent/nodejs-rtorrent && npm install"
 
 echo ""
-echo -e "$COL_YELLOW NodeJS-rTorrent Installed $COL_RESET"
+echo -e "$COL_BLUE NodeJS-rTorrent Installed $COL_RESET"
 echo ""
 
 cp rtorrent.conf /etc/supervisor/conf.d/rtorrent.conf
@@ -64,10 +59,17 @@ cp njr.conf /etc/supervisor/conf.d/njr.conf
 sed -i '/chmod=0700/a chown=rtorrent:rtorrent' /etc/supervisor/supervisord.conf
 
 supervisorctl reload > /dev/null
-echo -e "$COL_YELLOW Supervisor Configured $COL_RESET"
+echo -e "$COL_BULE Supervisor Configured $COL_RESET"
 supervisorctl status
 
 echo ""
 echo "## > Login Details ##"
 echo "URL: http://"$host":3000"
 echo "User: "$user" Password: "$pw""
+echo ""
+echo "Controls"
+echo "supervisorctl status"
+echo "supervisorctl start"
+echo "supervisorctl stop"
+echo "supervisorctl start"
+echo ""
